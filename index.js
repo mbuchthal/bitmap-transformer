ad5a981936243a7345df3ef3f510eaa5376ed87c
Mark Buchthal <mark.buchthal@gmail.com>
2016-04-10 16:23:48 -0700

    add basic events

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
 const app = require('./lib/app.js');
 const writeFile = require('./lib/write_bitmap');
-const bufferTransform = require('./lib/bitmap_transform');
+const BufferTransform = require('./lib/bitmap_transform');
+const bitmapToObject = require('./lib/bitmap_object');
+const EE = require('events');
+
+var ee = new EE();
+var transform = new BufferTransform();
 
 var myFile = __dirname + '/palette-bitmap.bmp';
 app.readFile(myFile);
+
+
+
